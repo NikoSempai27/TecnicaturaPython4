12e7860d720e358052e04c6a8d6f29c451c74adc
Pablo Nicolas Guala <112034712+NikoSempai27@users.noreply.github.com>
2023-11-08 20:05:07 -0300

    Clase 2 de Python Cuarto Semestre
    Clase 01: Sistema Numérico, binario, decimal, octal, hexadecimal. Lo aplicamos en los tipo float, también vimos el modulo math y decimal para valores infinitos positivos y negativos

---
+++ Clase01/tipo_float.py
@@ -0,0 +1,20 @@
+# Profundizamos en el tipo float
+a = 3.0
+print(f'a: {a:.2f}')
+
+# Constructor de tipo float -> puede recibir entero y tipo string
+a = float(10) # Le pasamos un tipo entero (int)
+a = float('10')
+print(f'a: {a:.2f}')
+
+# Notacion exponencial (valores positivos o negativos)
+a = 3e5
+print(f'a: {a:.2f}')
+
+a = 3e-5
+print(f'a: {a:.5f}')
+
+# Cualquier calculo que incluya un float, todo cambia a float
+a = 4.0 + 5
+print(a)
+print(type(a))
